1d82d6c3e577daba227e62269e739e49da2297c0
PruebaCristianTapia <c.tapia114@alumnos.santotomas.cl>
2023-10-28 20:48:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.scanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button scanButton;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scanButton= findViewById(R.id.scan_button);
+        resultTextView= findViewById(R.id.result_textview);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Iniciar la actividad de escaneo de ZXing
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setPrompt("Escanea un codigo de barras o QR");
+                integrator.initiateScan();
+
+            }
+        });
+
+
+    }
+}
